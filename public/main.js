5fef63c7d3111b157837830b42c96ff9a75fcab1
Tijn Kersjes <tijn@divbyzero.nl>
2015-04-01 01:42:49 +0200

    First proof of concept

---
+++ public/main.js
@@ -0,0 +1,73 @@
+var placesService, directionsService;
+var destinationName;
+var cafeName;
+
+function locateMe() {
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    searchForCoffee(position.coords.latitude, position.coords.longitude);
+  }, function (err) {
+    alert("Could not find your location. Using Utrecht instead.");
+    searchForCoffee(52.0833, 5.1167);
+  });
+}
+
+function searchForCoffee(lat, lng) {
+
+  myLocation = new google.maps.LatLng(lat, lng);
+
+  var map = new google.maps.Map(document.getElementById("map-canvas"), {
+    center: myLocation,
+    zoom: 10
+  });
+
+  placesService = new google.maps.places.PlacesService(map);
+  directionsService = new google.maps.DirectionsService(map);
+
+  var searchRequest = {
+    location: myLocation,
+    radius: 1000,
+    types: ["cafe"],
+    keyword: "espresso",
+    openNow: true
+  };
+
+  placesService.nearbySearch(searchRequest, foundCoffee);
+}
+
+function foundCoffee(results, status) {
+
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+    destinationName = results[0].name;
+
+    var routeRequest = {
+      origin: myLocation,
+      destination: results[0].geometry.location,
+      travelMode: google.maps.TravelMode.WALKING
+    };
+
+    directionsService.route(routeRequest, foundRoute);
+  }
+}
+
+
+function foundRoute(results, status) {
+
+  if (status == google.maps.DirectionsStatus.OK) {
+    var route = results.routes[0].legs[0];
+    var eta = Math.floor(route.duration.value / 60);
+
+    var list = [];
+
+    for (var i = 0; i < route.steps.length; i++) {
+      var step = route.steps[i];
+      var dist = "<i>" + step.distance.value + " m</i>";
+      list.push(step.instructions + " " + dist);
+    }
+
+    document.getElementById("instructions").innerHTML = "<div class='title'>" + destinationName + " (" + eta + " minutes)</div>" + "<ul><li>" + list.join("</li><li>") + "</li></ul>";
+  }
+}
+
+google.maps.event.addDomListener(window, "load", locateMe);

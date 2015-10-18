19ce8a2af08bff2b83baab0b7f25df26e8e6db02
Tijn Kersjes <tijn@divbyzero.nl>
2015-10-18 02:02:42 +0200

    Toggle between walking and driving

--- public/main.js
+++ public/main.js
@@ -1,14 +1,30 @@
-var placesService, directionsService;
 var destinationName;
 var cafeName;
+var mode = "WALKING";
+
+var map, placesService, directionsService;
+
+function init () {
+     map = new google.maps.Map(document.getElementById("map-canvas"), {
+      center: new google.maps.LatLng(0, 0),
+      zoom: 10
+    });
+
+     placesService = new google.maps.places.PlacesService(map);
+     directionsService = new google.maps.DirectionsService(map);
+
+    locateMe();
+}
 
 function locateMe() {
+
+  document.getElementById("instructions").innerHTML = "<div class='title busy'>A little wait...</div>";
 
   navigator.geolocation.getCurrentPosition(function (position) {
     searchForCoffee(position.coords.latitude, position.coords.longitude);
   }, function (err) {
     searchForCoffee(52.0833, 5.1167);
-    document.getElementById("instructions").innerHTML = "<div class='title warn'>Unable to locate you</div>";
+    document.getElementById("instructions").innerHTML = "<div class='title warn'>Unable to locate you " + modeButton() + "</div>";
   });
 }
 
@@ -16,17 +32,9 @@ function searchForCoffee(lat, lng) {
 
   myLocation = new google.maps.LatLng(lat, lng);
 
-  var map = new google.maps.Map(document.getElementById("map-canvas"), {
-    center: myLocation,
-    zoom: 10
-  });
-
-  placesService = new google.maps.places.PlacesService(map);
-  directionsService = new google.maps.DirectionsService(map);
-
   var searchRequest = {
     location: myLocation,
-    radius: 1000,
+    radius: mode === "WALKING" ? 1000 : 5000,
     types: ["cafe"],
     keyword: "espresso",
     openNow: true
@@ -44,20 +52,20 @@ function foundCoffee(results, status) {
     var routeRequest = {
       origin: myLocation,
       destination: results[0].geometry.location,
-      travelMode: google.maps.TravelMode.WALKING
+      travelMode: google.maps.TravelMode[mode]
     };
 
     directionsService.route(routeRequest, foundRoute);
   }
   else {
-    document.getElementById("instructions").innerHTML = "<div class='title warn'>No coffee found nearby</div>";
+    document.getElementById("instructions").innerHTML = "<div class='title warn'>No coffee found nearby " + modeButton() + "</div>";
   }
 }
-
 
 function foundRoute(results, status) {
 
   if (status == google.maps.DirectionsStatus.OK) {
+
     var route = results.routes[0].legs[0];
     var eta = Math.floor(route.duration.value / 60);
 
@@ -65,15 +73,29 @@ function foundRoute(results, status) {
 
     for (var i = 0; i < route.steps.length; i++) {
       var step = route.steps[i];
-      var dist = "<i>" + step.distance.value + " m</i>";
+      var dist = "<i>" + Math.round(step.distance.value / 50) * 50 + " m</i>";
       list.push(step.instructions + " " + dist);
     }
 
-    document.getElementById("instructions").innerHTML = "<div class='title'>" + destinationName + " (" + eta + " minutes)</div>" + "<ul><li>" + list.join("</li><li>") + "</li></ul>";
+    document.getElementById("instructions").innerHTML = "<div class='title'>" + destinationName + " (" + eta + " minutes) " + modeButton() + "</div>" + "<ul><li>" + list.join("</li><li>") + "</li></ul>";
   }
   else {
-    document.getElementById("instructions").innerHTML = "<div class='title warn'>No route found to " + destinationName + "</div>";
+    document.getElementById("instructions").innerHTML = "<div class='title warn'>No route found to " + destinationName + " " + modeButton() + "</div>";
   }
 }
 
-google.maps.event.addDomListener(window, "load", locateMe);
+function toggleMode() {
+    if (mode === "WALKING") {
+        mode = "DRIVING";
+    }
+    else {
+        mode = "WALKING";
+    }
+    locateMe();
+}
+
+function modeButton() {
+    return "<a href='#' onclick='toggleMode(); return false;'>" + (mode === "WALKING" ? "Drive" : "Walk") + " instead</a>";
+}
+
+google.maps.event.addDomListener(window, "load", init);
